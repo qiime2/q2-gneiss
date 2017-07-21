c38874c0305d8fafa0b6f07bfbf4d6cff80fa901
Evan Bolyen <ebolyen@gmail.com>
2017-07-21 10:55:55 -0700

    CI/CD: set up for automated builds (#4)

--- setup.py
+++ setup.py
@@ -6,31 +6,25 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 from setuptools import setup, find_packages
-import re
 
-# version parsing from __init__ pulled from Flask's setup.py
-# https://github.com/mitsuhiko/flask/blob/master/setup.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
+import versioneer
 
 setup(
     name="q2-gneiss",
-    version='0.0.1',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=find_packages(),
-    # pandas and q2-dummy-types are only required for the dummy methods and
-    # visualizers provided as examples. Remove these dependencies when you're
-    # ready to develop your plugin, and add your own dependencies (if there are
-    # any).
-    install_requires=['pandas', 'numpy', 'gneiss>=0.3.0'],
     author="Jamie Morton",
     author_email="jamietmorton@gmail.com",
     description="Compositional Data Analysis and Visualization Toolbox",
     license='BSD-3-Clause',
-    url="https://github.com/biocore/gneiss",
-    extra_require={
-        'q2': ['qiime2 >= 2017.2.0', 'biom-format', 'seaborn']
-    },
+    url="https://github.com/qiime2/q2-gneiss",
     entry_points={
         'qiime2.plugins': ['q2-gneiss=q2_gneiss.plugin_setup:plugin']
-    }
+    },
+    package_data={
+        'q2_gneiss.cluster.tests': ['data/*'],
+        'q2_gneiss.regression.tests': ['data/*'],
+    },
+    zip_safe=False,
 )
