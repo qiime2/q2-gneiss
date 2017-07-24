2cc01d6cae17f29d9b5b67afb45cc174900c903e
ebolyen <ebolyen@gmail.com>
2017-07-24 11:36:02 -0700

    MAINT: port add-pseudocount and update type imports

--- q2_gneiss/composition/_composition.py
+++ q2_gneiss/composition/_composition.py
@@ -5,12 +5,32 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-from q2_composition.plugin_setup import Composition
 from q2_types.tree import Hierarchy
 from q2_gneiss.plugin_setup import plugin
-from q2_composition._type import Balance
-from q2_types.feature_table import FeatureTable
+from q2_types.feature_table import (FeatureTable, Frequency, Composition,
+                                    Balance)
+from q2_gneiss.composition._impute import add_pseudocount
 from gneiss.composition import ilr_transform
+from qiime2.plugin import Int
+
+
+plugin.methods.register_function(
+    function=add_pseudocount,
+    inputs={'table': FeatureTable[Frequency]},
+    parameters={'pseudocount': Int},
+    outputs=[('composition_table', FeatureTable[Composition])],
+    input_descriptions={
+        'table': 'The feature table to which pseudocounts should be added.'
+    },
+    parameter_descriptions={
+        'pseudocount': 'The value to add to all counts in the feature table.'
+    },
+    output_descriptions={
+        'composition_table': 'The resulting feature table.'
+    },
+    name='Add pseudocount to table',
+    description="Increment all counts in table by pseudocount."
+)
 
 
 plugin.methods.register_function(
--- q2_gneiss/plot/_plot.py
+++ q2_gneiss/plot/_plot.py
@@ -18,8 +18,7 @@ from gneiss.plot._decompose import balance_barplots, balance_boxplot
 from gneiss.util import (match, NUMERATOR, DENOMINATOR)
 
 from q2_types.tree import Hierarchy
-from q2_composition.plugin_setup import Composition, Balance
-from q2_types.feature_table import FeatureTable
+from q2_types.feature_table import FeatureTable, Composition, Balance
 from q2_types.feature_data import FeatureData, Taxonomy
 from qiime2.plugin import Int, MetadataCategory, Str, Choices
 
