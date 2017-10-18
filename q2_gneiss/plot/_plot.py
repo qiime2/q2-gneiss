64a20b1968a85dbed8439595b81b8681ed877dc1
Jamie Morton <jamietmorton@gmail.com>
2017-10-17 17:28:59 -0700

    ENH: Adding in proportion plots
    This also changes the API for balance taxonomy, since it now requires a composition
    table as input instead of balances as input.
    
    This also adds in a parameter `threshold` that discretizes continuous metadata to
    plot in the proportion plot

--- q2_gneiss/plot/_plot.py
+++ q2_gneiss/plot/_plot.py
@@ -6,6 +6,7 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 import os
+import warnings
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,25 +15,30 @@ from skbio.stats.composition import clr, centralize
 
 from q2_gneiss.plugin_setup import plugin
 from gneiss.plot._heatmap import heatmap
-from gneiss.plot._decompose import balance_barplots, balance_boxplot
+from gneiss.plot._decompose import (balance_barplots, balance_boxplot,
+                                    proportion_plot)
 from gneiss.util import (match, match_tips, NUMERATOR, DENOMINATOR)
 
 from q2_types.tree import Hierarchy
 from q2_types.feature_table import FeatureTable, Composition, Balance
 from q2_types.feature_data import FeatureData, Taxonomy
-from qiime2.plugin import Int, MetadataCategory, Str, Choices
+from qiime2.plugin import Int, MetadataCategory, Str, Choices, Float
 
 
-def balance_taxonomy(output_dir: str, balances: pd.DataFrame, tree: TreeNode,
+def balance_taxonomy(output_dir: str, table: pd.DataFrame, tree: TreeNode,
                      taxonomy: pd.DataFrame,
                      balance_name: Str,
                      taxa_level: Int = 0,
+                     n_features: Int = 10,
+                     threshold: Float = None,
                      metadata: MetadataCategory = None) -> None:
+
+    # make sure that the table and tree match up
+    table, tree = match_tips(table, tree)
 
     # parse out headers for taxonomy
     taxa_data = list(taxonomy['Taxon'].apply(lambda x: x.split(';')).values)
-    taxa_df = pd.DataFrame(taxa_data,
-                           index=taxonomy.index)
+    taxa_df = pd.DataFrame(taxa_data, index=taxonomy.index)
 
     # fill in NAs
     def f(x):
@@ -49,17 +55,29 @@ def balance_taxonomy(output_dir: str, balances: pd.DataFrame, tree: TreeNode,
         num_features = pd.DataFrame(
             {num_clade.name: taxa_df.loc[num_clade.name]}
             ).T
+        r = 1
     else:
         num_features = taxa_df.loc[num_clade.subset()]
+        r = len(list(num_clade.tips()))
 
     if denom_clade.is_tip():
         denom_features = pd.DataFrame(
             {denom_clade.name: taxa_df.loc[denom_clade.name]}
             ).T
+        s = 1
     else:
         denom_features = taxa_df.loc[denom_clade.subset()]
+        s = len(list(denom_clade.tips()))
 
-    num_color, denom_color = '#4c72b0', '#4c72b0'
+    balances = (np.log(table.loc[:, num_features.index]).mean(axis=1) -
+                np.log(table.loc[:, denom_features.index]).mean(axis=1))
+
+    balances = balances * np.sqrt(r * s / (r + s))
+    balances = pd.DataFrame(balances, index=table.index, columns=[balance_name])
+
+    # the actual colors for the numerator and denominator
+    num_color = '#105d33'
+    denom_color = '#b0d78e'
 
     fig, (ax_num, ax_denom) = plt.subplots(2)
     balance_barplots(tree, balance_name, taxa_level, taxa_df,
@@ -67,38 +85,97 @@ def balance_taxonomy(output_dir: str, balances: pd.DataFrame, tree: TreeNode,
                      axes=(ax_num, ax_denom))
 
     ax_num.set_title(
-        r'$%s_{numerator} \; taxa \; (%d \; taxa)$' % (balance_name,
-                                                       len(num_features)))
+        r'$%s_{numerator} \; taxa \; (%d \; taxa)$' % (
+            balance_name, len(num_features)))
     ax_denom.set_title(
-        r'$%s_{denominator} \; taxa \; (%d \; taxa)$' % (balance_name,
-                                                         len(denom_features)))
+        r'$%s_{denominator} \; taxa \; (%d \; taxa)$' % (
+            balance_name, len(denom_features)))
     ax_denom.set_xlabel('Number of unique taxa')
     plt.tight_layout()
     fig.savefig(os.path.join(output_dir, 'barplots.svg'))
     fig.savefig(os.path.join(output_dir, 'barplots.pdf'))
 
+    dcat = None
     if metadata is not None:
         fig2, ax = plt.subplots()
         c = metadata.to_series()
         data, c = match(balances, c)
         data[c.name] = c
         y = data[balance_name]
+
         # check if continuous
         try:
             c = c.astype(np.float64)
             ax.scatter(c.values, y)
             ax.set_xlabel(c.name)
+            if threshold is None:
+                threshold = c.mean()
+            dcat = c.apply(
+                lambda x: '%s < %f' % (c.name, threshold)
+                if x < threshold
+                else '%s > %f' % (c.name, threshold)
+            )
+
         except:
             balance_boxplot(balance_name, data, y=c.name, ax=ax)
+            if len(c.value_counts()) > 2:
+                warnings.warn(
+                    ('More than 2 categories detected.  '
+                     'Proportion plots will not be displayed'),
+                    stacklevel=2)
+            else:
+                dcat = c
 
-        ylabel = (r"$%s = \ln \frac{%s_{numerator}}"
-                  "{%s_{denominator}}$") % (balance_name,
-                                            balance_name,
-                                            balance_name)
-        ax.set_title(ylabel, rotation=0)
-        ax.set_ylabel('log ratio')
-        fig2.savefig(os.path.join(output_dir, 'balance_metadata.svg'))
-        fig2.savefig(os.path.join(output_dir, 'balance_metadata.pdf'))
+        if dcat is not None:
+            ylabel = (r"$%s = \ln \frac{%s_{numerator}}"
+                      "{%s_{denominator}}$") % (balance_name,
+                                                balance_name,
+                                                balance_name)
+            ax.set_title(ylabel, rotation=0)
+            ax.set_ylabel('log ratio')
+            fig2.savefig(os.path.join(output_dir, 'balance_metadata.svg'))
+            fig2.savefig(os.path.join(output_dir, 'balance_metadata.pdf'))
+
+            # Proportion plots
+            # first sort by clr values and calculate average fold change
+            ctable = pd.DataFrame(clr(centralize(table)),
+                                  index=table.index, columns=table.columns)
+            fold_change = ctable.sum(axis=0)
+            fold_change = fold_change.sort_values()
+
+            metadata = pd.DataFrame({dcat.name: dcat})
+            fig3, (ax_denom, ax_num) = plt.subplots(1, 2)
+            proportion_plot(table, metadata,
+                            category=metadata.columns[0],
+                            left_group=dcat.value_counts().index[0],
+                            right_group=dcat.value_counts().index[1],
+                            feature_metadata=taxa_df,
+                            label_col=taxa_level,
+                            num_features=fold_change.index[-n_features:],
+                            denom_features=fold_change.index[:n_features],
+                            num_color=num_color,
+                            denom_color=denom_color,
+                            axes=(ax_num, ax_denom))
+            fig3.subplots_adjust(
+                # the left side of the subplots of the figure
+                left= 0.3,
+                # the right side of the subplots of the figure
+                right=0.9,
+                # the bottom of the subplots of the figure
+                bottom=0.1,
+                # the top of the subplots of the figure
+                top=0.9,
+                # the amount of width reserved for blank space
+                # between subplots
+                wspace=0,
+                # the amount of height reserved for white space
+                # between subplots
+                hspace=0.2,
+            )
+
+
+            fig3.savefig(os.path.join(output_dir, 'proportion_plot.svg'))
+            fig3.savefig(os.path.join(output_dir, 'proportion_plot.pdf'))
 
     index_fp = os.path.join(output_dir, 'index.html')
     with open(index_fp, 'w') as index_f:
@@ -108,6 +185,13 @@ def balance_taxonomy(output_dir: str, balances: pd.DataFrame, tree: TreeNode,
             index_f.write(('<img src="balance_metadata.svg" '
                            'alt="barplots">\n\n'
                            '<a href="balance_metadata.pdf">'
+                           'Download as PDF</a><br>\n'))
+
+        if dcat is not None:
+            index_f.write('<h1>Proportion Plot </h1>\n')
+            index_f.write(('<img src="proportion_plot.svg" '
+                           'alt="proportionss">\n\n'
+                           '<a href="proportion_plot.pdf">'
                            'Download as PDF</a><br>\n'))
 
         index_f.write(('<h1>Balance Taxonomy</h1>\n'
@@ -130,20 +214,28 @@ def balance_taxonomy(output_dir: str, balances: pd.DataFrame, tree: TreeNode,
 
 plugin.visualizers.register_function(
     function=balance_taxonomy,
-    inputs={'balances': FeatureTable[Balance], 'tree': Hierarchy,
+    inputs={'table': FeatureTable[Composition], 'tree': Hierarchy,
             'taxonomy': FeatureData[Taxonomy]},
     parameters={'balance_name': Str,
                 'taxa_level': Int,
-                'metadata': MetadataCategory},
+                'metadata': MetadataCategory,
+                'n_features': Int,
+                'threshold': Float,
+    },
     input_descriptions={
-        'balances': 'The table of balances resulting from the ilr transform.',
+        'table': 'A table of compositions.',
         'tree': 'The tree used to calculate the balances.',
-        'taxonomy': 'Taxonomy information for the OTUs.',
+        'taxonomy': 'Taxonomy information for the OTUs.'
     },
     parameter_descriptions={
         'balance_name': 'Name of the balance to summarize.',
         'taxa_level': 'Level of taxonomy to summarize.',
-        'metadata': 'Metadata for plotting the balance (optional).'},
+        'metadata': 'Metadata for plotting the balance (optional).',
+        'n_features': 'The number of features to plot in the proportion plot.',
+        'threshold': ('A threshold to designate discrete categories '
+                      'for continuous metadata.  If not specified, this threshold '
+                      'will default to the mean.')
+    },
     name='Balance Summary',
     description=("Visualize the distribution of a single balance "
                  "and summarize its numerator and denominator components.")
