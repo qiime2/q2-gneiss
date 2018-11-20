8852c199dfd73bec3f3a24684f2cd0e95446a5cf
James Morton <mortonjt@Lisa-MacBook-Pro-2.local>
2018-11-20 09:52:00 -0800

    TST: updating new test and ditching old test

--- q2_gneiss/composition/tests/test_method.py
+++ q2_gneiss/composition/tests/test_method.py
@@ -79,39 +79,17 @@ class TestILRTransform(unittest.TestCase):
                              columns=['a', 'b', 'c', 'd'])
         table = table.reindex(columns=np.random.permutation(table.columns))
         tree = TreeNode.read([
-            '((c:0.025,d:0.025,e:0.025):0.2,(b:0.025,a:0.025):0.2);'])
+            '((c:0.025,d:0.025,f:0.1,e:0.025):0.2,(b:0.025,a:0.025):0.2);'])
         res_balances, res_tree = ilr_phylogenetic(table, tree)
         exp_balances = pd.DataFrame(
             [[0.693147, 0.0, 3.892122e-17],
              [0.0, -4.901291e-01, -4.901291e-01],
              [-0.693147, -5.551115e-17, -3.892122e-17]],
-            columns=['y0', 'y2', 'y3'],
+            columns=['y0', 'y1', 'y2'],
             index=[1, 2, 3])
         pdt.assert_frame_equal(res_balances, exp_balances)
-        exp_tree_str = ('((e:0.025,(c:0.025,d:0.025)y3)y1:0.2,'
-                        '(b:0.025,a:0.025)y2:0.2)y0;\n')
-        self.assertEqual(str(res_tree), exp_tree_str)
-
-    def test_ilr_phylogenetic_match(self):
-        np.random.seed(0)
-        table = pd.DataFrame([[1, 1, 2, 2],
-                              [1, 2, 2, 1],
-                              [2, 2, 1, 1]],
-                             index=[1, 2, 3],
-                             columns=['a', 'b', 'c', 'd'])
-        table = table.reindex(columns=np.random.permutation(table.columns))
-        tree = TreeNode.read([
-            '((c:0.025,d:0.025,f:0.1 e:0.025):0.2,(b:0.025,a:0.025):0.2);'])
-        res_balances, res_tree = ilr_phylogenetic(table, tree)
-        exp_balances = pd.DataFrame(
-            [[0.693147, 0.0, 3.892122e-17],
-             [0.0, -4.901291e-01, -4.901291e-01],
-             [-0.693147, -5.551115e-17, -3.892122e-17]],
-            columns=['y0', 'y2', 'y3'],
-            index=[1, 2, 3])
-        pdt.assert_frame_equal(res_balances, exp_balances)
-        exp_tree_str = ('((e:0.025,(c:0.025,d:0.025)y3)y1:0.2,'
-                        '(b:0.025,a:0.025)y2:0.2)y0;\n')
+        exp_tree_str = ('((b:0.025,a:0.025)y1:0.2,'
+                        '(c:0.025,d:0.025)y2:0.2)y0;\n')
         self.assertEqual(str(res_tree), exp_tree_str)
 
 
